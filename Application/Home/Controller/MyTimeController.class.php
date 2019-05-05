bf5d02d1422e43fbeee9201329e7ee42b4e5084a
qunyun <2467060526@qq.com>
2019-05-05 12:08:07 +0800

    tj

--- Application/Home/Controller/MyTimeController.class.php
+++ Application/Home/Controller/MyTimeController.class.php
@@ -3,8 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class MyTimeController extends Controller {
     public function index(){
+        //抢购产品
         $this->goods=M('goodsInfo')
-            ->field('id,goodsname,price,buynum,buytime,buyprice')
+            ->field('id,goodsname,price,buynum,buytime,buyprice,pic')
             ->where('is_buy=1')
             ->select();
         $this->display();
