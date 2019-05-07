b69665fb916bd7388983aff881bab285ecc98147
cy <397607580@qq.com>
2019-05-07 09:37:04 +0800

    第二次提交

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,26 +2,71 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+    //首页遍历
+
     public function index(){
         $this->goods=M('goods_ad')
             ->where('ad_place=7')
             ->select();
 
         //$this->assign('goods',$goods);
-        $this->goods_info=M('goods_info')
-            ->field('goodsname,price,pic')
+        //一层
+        $this->goods_info=M('tp_goodsinfo')
+            ->field('goodsname,price,pic,sale,id')
+            //->where('tid=5')
+            ->limit(7)
+            ->select();
+        //二层
+        $this->goodsy=M('tp_goodsinfo')
+            ->field('goodsname,price,pic,sale,id')
+            //->where('tid=5')
+            ->limit(7)
+            ->select();
+        //三层
+        $this->goodsyf=M('tp_goodsinfo')
+            ->field('goodsname,price,pic,sale,id')
             ->limit(7)
             ->select();
 
-        $this->goodsy=M('goods_info')
-            ->field('goodsname,price,pic')
-            ->where('id=148')
+        //四层
+        $this->goodssf=M('tp_goodsinfo')
+            ->field('goodsname,price,pic,sale,id')
+            //->where('tid=4')
+            ->limit(7)
             ->select();
-        $this->goodsyf=M('goods_info')
-            ->field('goodsname,price,pic')
-            ->where('id=147')
+        //五层
+        $this->goodsList=M('tp_goodsinfo')
+            ->field('goodsname,price,pic,sale,id')
+            ->limit(20)
             ->select();
         $this->display();
+        //商品详情
+
+
+    }
+    public function goodsdata(){
+         $id=I('get.id');
+        $goodimg=M('tp_goodsimages')
+            ->field('pics')
+            ->where("goodsid=$id")
+            ->select();
+           // dump($goodimg);
+        $goods=M('tp_goodsinfo')
+            ->field('goodsname,price,sale')
+            ->where("id=$id")
+            ->select();
+        $this->goodsyf=M('tp_goodsinfo')
+            ->field('goodsname,price,pic,sale,id')
+            ->limit(7)
+            ->select();
+        //为你推荐
+        $this->goodsList=M('tp_goodsinfo')
+            ->field('goodsname,price,pic,sale,id')
+            ->limit(20)
+            ->select();
+        $this->assign('goods',$goods);
+        $this->assign('goodimg',$goodimg);
+        $this->display("index/ui-product");
 
     }
 }
