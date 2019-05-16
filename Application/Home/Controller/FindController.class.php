8cdbe51ce29805c6d371ececc0e0525a99abd25b
qunyun <2467060526@qq.com>
2019-05-16 18:24:53 +0800

    zcy

---
+++ Application/Home/Controller/FindController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+/**
+ * 分类
+ * Class TypeController
+ * @package Home\Controller
+ */
+class FindController extends Controller {
+    public function index(){
+        $this->display();
+    }
+}
---
+++ Application/Home/Controller/PersonalController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+/**
+ * 分类
+ * Class TypeController
+ * @package Home\Controller
+ */
+class PersonalController extends Controller {
+    public function index(){
+       $this->display();
+    }
+
+}
